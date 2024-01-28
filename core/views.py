db01bbe45520c1f39458280654b620034409809b
Hamidusman <abdulhamidusman218@gmail.com>
2024-01-28 15:40:07 +0000

    student profile

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def students(request):
+    return render(request, 'students.html')
