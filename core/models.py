62255aae762fb465e6aa71ed0ffac27189b9b358
Hamidusman <abdulhamidusman218@gmail.com>
2024-01-29 18:54:34 +0000

    student model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    fullname = models.CharField(max_length = 100)
+    nickname = models.CharField( max_length=100 default ='')
+    image = models.ImageField(upload_to='student_images/')
+    about = models.TextField(max_length = 10000)
+    location = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 11)
+    
